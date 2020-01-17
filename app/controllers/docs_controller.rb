a6f9c21562d1c0a5085741873ae64ab2c9f71f7c
Myousry <mohamedyousry250@gmail.com>
2020-01-17 17:27:20 +0200

    change the index of docs to display the docs of logged user

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,7 +1,7 @@
 class DocsController < ApplicationController
 	before_action :find_doc , only: [:show , :edit , :update , :destroy]
 	def index
-		@docs = Doc.all.order("created_at DESC")
+		@docs = Doc.where(user_id: current_user)
 	end
 
 	def show 
@@ -9,7 +9,7 @@ class DocsController < ApplicationController
 	end
 
 	def new
-		@doc = current_user.docs.build
+
 	end
 
 	def create
